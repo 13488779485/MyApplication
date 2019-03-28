116fc4a2ac4fcc64a2225533a67efd459f4ff1e8
lizeyuan02 <lizeyuan02@baidu.com>
2019-03-28 14:56:05 +0800

    第一次提交

---
+++ app/src/main/java/com/example/lizeyuan/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lizeyuan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        String string = new String("");
+
+
+        string.charAt(0);
+
+
+
+
+    }
+}
