5f8f654e523eb2b00dd62d697c198aa0801ab3ef
lizeyuan02 <lizeyuan02@baidu.com>
2019-03-28 15:35:12 +0800

    第三次提交

--- app/src/main/java/com/example/lizeyuan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lizeyuan/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         String string = new String("");
         string = new String("第二次提交");
         string = new String("第三次提交");
+        string = new String("第三次提交");
 
 
         string.charAt(0);
