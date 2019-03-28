62054bb20f1346f6df876730edd07c8b33d6a0f7
lizeyuan02 <lizeyuan02@baidu.com>
2019-03-28 17:43:26 +0800

    分支2第一次提交

--- app/src/main/java/com/example/lizeyuan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lizeyuan/myapplication/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         string = new String("第三次提交");
         string = new String("第四次提交");
         string = new String("分支master第1次提交");
+        string = new String("分支2第1次提交");
 
 
 
