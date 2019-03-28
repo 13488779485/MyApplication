45a024de39176a620218ea3b44870fc1d9776726
lizeyuan02 <lizeyuan02@baidu.com>
2019-03-28 17:25:13 +0800

    分支1第一次提交

--- app/src/main/java/com/example/lizeyuan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lizeyuan/myapplication/MainActivity.java
@@ -15,10 +15,12 @@ public class MainActivity extends AppCompatActivity {
         string = new String("第二次提交");
         string = new String("第三次提交");
         string = new String("第四次提交");
+        string = new String("分支1第1次提交");
         string = new String("分支master第1次提交");
         string = new String("分支2第1次提交");
         string = new String("分支2第2次提交");
         string = new String("分支2第3次提交");
+        // rebase完成
 
 
 
